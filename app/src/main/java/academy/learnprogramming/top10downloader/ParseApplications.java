2363940480c429a3936af2e5a4bd931ce385fc5b
Ervin Szekely <ervin.szekely88@gmail.com>
2018-06-27 22:20:12 +0200

    Testing commit

--- app/src/main/java/academy/learnprogramming/top10downloader/ParseApplications.java
+++ app/src/main/java/academy/learnprogramming/top10downloader/ParseApplications.java
@@ -38,7 +38,7 @@ public class ParseApplications {
                 String tagName = xpp.getName();
                 switch (eventType) {
                     case XmlPullParser.START_TAG:
-                        Log.d(TAG, "parse: Starting tag for " + tagName);
+//                        Log.d(TAG, "parse: Starting tag for " + tagName);
                         if ("entry".equalsIgnoreCase(tagName)) {
                             inEntry = true;
                             currentRecord = new FeedEntry();
@@ -48,7 +48,7 @@ public class ParseApplications {
                         textValue = xpp.getText();
                         break;
                     case XmlPullParser.END_TAG:
-                        Log.d(TAG, "parse: Ending tag for " + tagName);
+//                        Log.d(TAG, "parse: Ending tag for " + tagName);
                         if (inEntry) {
                             if ("entry".equalsIgnoreCase(tagName)) {
                                 applications.add(currentRecord);
@@ -72,8 +72,8 @@ public class ParseApplications {
                 eventType = xpp.next();
             }
             for (FeedEntry app : applications) {
-                Log.d(TAG, "**************");
-                Log.d(TAG, app.toString());
+//                Log.d(TAG, "**************");
+//                Log.d(TAG, app.toString());
             }
         } catch (Exception e) {
             status = false;
